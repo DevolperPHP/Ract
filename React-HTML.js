ca5b89a8b4e420cc44634fa4255f07ae2045b259
DevolperPHP <devolperphp@gmail.com>
2021-07-13 20:00:13 +0300

    Create React-HTML.js

---
+++ React-HTML.js
@@ -0,0 +1,18 @@
+// Call React library
+import React from 'react';
+
+// Opent a function to start coding as HTML
+
+function App(){
+  return (
+    // Here you can coding as HTNL
+    
+    <h1>Hello World</h1>
+  )
+}
+
+// Them export the function
+
+export default App;
+
+// And Check the result
